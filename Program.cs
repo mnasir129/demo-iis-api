8c492096908dc53ec94ebdf3929e2cd31c7ac956
Muhammad Nasir <awesomepkinfo@gmail.com>
2026-06-10 09:37:19 +0400

    added new comment

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ app.MapGet("/health", () => Results.Ok(new
 {
     status = "Healthy",
     application = "DemoIisApi",
-    deployedBy = "Jenkins + Docker + Ansible +meto",
+    deployedBy = "Jenkins + Docker + Ansible +metooo",
     timestamp = DateTime.UtcNow
 }));
 
